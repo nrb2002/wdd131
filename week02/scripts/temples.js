74ad0327c73d22d6cb43182925ab7a69efe86892
nrb2002 <tshibasubaron@yahoo.fr>
2025-05-22 18:27:06 +0100

    Some fixes included

--- week02/scripts/temples.js
+++ week02/scripts/temples.js
@@ -1,7 +1,34 @@
 const hamButton = document.getElementById('hambMenu');
-const nav = document.querySelector('nav');
+const ul = document.querySelector('ul');
 
 hamButton.addEventListener('click', () => {
-	nav.classList.toggle('open');
+	ul.classList.toggle('open');
 	hamButton.classList.toggle('open');
+});
+
+// Handle responsive display
+// function updateNavDisplay() {
+//   if (window.innerWidth > 512) {
+//     ul.classList.add('flex-display');
+//     ul.classList.remove('open');
+//     hamButton.classList.remove('open');
+//   } else {
+//     ul.classList.remove('flex-display');
+//   }
+// }
+
+// // Run on page load
+// updateNavDisplay();
+
+// // Run on resize
+// window.addEventListener('resize', updateNavDisplay);
+
+window.addEventListener('resize', () =>{
+	if (window.innerWidth > 512) {
+    ul.classList.add('flex-display');
+    ul.classList.remove('open');
+    hamButton.classList.remove('open');
+  } else {
+    ul.classList.remove('flex-display');
+  }
 });
